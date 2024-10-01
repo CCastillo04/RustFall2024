709789fe88aa420501852107dd25e1edee90f7cb
CCastillo04 <115593074+CCastillo04@users.noreply.github.com>
2024-10-01 01:04:18 +0000

    Completed Attendance Verf: I/O Operation

--- Hello_World/src/main.rs
+++ Hello_World/src/main.rs
@@ -1,144 +1,58 @@
+use std::io::{self,Read, Write};
+use std::fs::File;
 
-//assignment 1
-   fn fahrenheit_to_celsius(f: f64){
+struct Car {
+    model: String,
+    year: u32,
+}
 
-       let fc: f64 = f.clone();
+fn readconsole_tofile() {
+    let mut file = File::create("user_info.txt").unwrap();
 
-       let _c: f64 = (fc-32.0)*(5.0/9.0);
+    let mut new_line = String::new();
 
-       println!("was {} in fahrenheit but now {:.2} in Celsius",f, _c);
-   }
+    print!("Whats your car model? ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut new_line).unwrap();
+    let model = new_line.trim().to_string();
+    new_line.clear();
 
-   fn celsius_to_fahrenheit(c: f64){
+    print!("What year is it?");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut new_line).unwrap();
+    let year: u32 = new_line.trim().parse().unwrap();
 
-       let cc: f64 = c.clone();
+    let _car = Car {model, year};
 
-       let _f: f64 = (cc*(9.0/5.0))+32.0;
-
-       println!("was {} in celsius {:.2} in Fahrenheit", c,_f);
-
-   }
-   fn main() {
-
-   const NUM: f64 = 32.0;
-   //declare constant
-   let mut num: f64 = 33.0;
-  //convert from f to c and c to f in two different functions
-   let mut count: i32 = 0;
- 
-   println!("{} is a freezing point in fahrenheit!", NUM);
-
-  //must have mutable variable -> convert to c
-  //loop to convert the next 5 numbers
-
-   loop{
-
-       fahrenheit_to_celsius(num);
-
-       num += 1 as f64;
-       count += 1;
-
-       if  count == 5{
-           break;
-       }
-   }
-
-  }
-
-//assignment 2
-  fn is_even(num: i32){
-      if num == 0 {
-          println!("{} is not even or odd", num);
-          return;
-      }
-      if num % 2 == 0 {
-          println!("{} is even", num);
-          return;
-      }
-
-      println!("{} is odd", num);
-
-      return;
-  }
-
-  fn main(){
-  //array of 10 int to return a function if it is even or false
-  let array: [i32;10] = [0,1,2,3,4,5,6,7,8,15];
-  let mut store: i32 = array[0];
-
-  let mut sum: i32 = 0;
-  //we also want the sum of the total array
-  //largest number
-  let mut cnt: usize = 0;
-  while cnt < array.len() {
-      is_even(array[cnt]);
-
-      if (array[cnt] % 3 == 0) && (array[cnt] % 5 == 0) {
-          println!("fizzbuzz");
-      }
-      else if array[cnt] % 3 == 0 {
-          println!("fizz");
-      }
-      else if array[cnt] % 5 == 0 {
-          println!("buzz");
-      }
-
-      sum += array[cnt];
-
-    
-      if store < array[cnt] {
-          store = array[cnt];
-      }
-
-      cnt+= 1;
-
-  }
-  println!("Total sum is: {}", sum);
-  println!("Largest number is: {}", store)
- }
-
-// //assignment 3 
-// //store secret number (hard code)
-// //check guess that returns 0 if correct, 1 if too high, -1 if too low
-// //must track the number of loops it took to
-// //}
-fn check_guess(guess: i32, secret: i32)-> i32{
-
-    if guess == secret{
-        return 0;
-    }else if guess > secret{
-        return 1;
-    }else{
-        return -1;
-    }
+    writeln!(file,"{}", _car.model).unwrap();
+    writeln!(file,"{}", _car.year).unwrap();
 
 }
+
+impl Car{
+
+    fn from_file(_path: &str) -> Car {
+        let mut file = File::open("user_info.txt").unwrap();
+        let mut new_line = String::new();
+
+        file.read_to_string(&mut new_line).unwrap();
+
+        let mut lines = new_line.lines();
+
+        let model = lines.next().unwrap().to_string();
+        let year = lines.next().unwrap().parse().unwrap();
+
+      Car { model, year }
+    }
+}
+
+fn readfile(){
+    let car = Car::from_file("user_info.txt");
+    println!("Model: {}",car.model);
+    println!("Year: {}", car.year)
+}
+
 fn main(){
-
-const SECRET: i32 = 24;
-
-let mut guess: i32 = 12;
-let mut _num: i32 = 0;
-
-loop{
-
-   let ans: i32 = check_guess(guess, SECRET);
-
-    if ans == 0
-    {
-        println!("{} was correct!", guess);
-        break;
-    }else if ans == 1{
-        println!("{} was too high", guess);
-        guess -=1;
-    }else{
-        println!("{} was too low", guess);
-        guess +=5;
-    }
-
-    _num+=1;
-}
-
-println!("Congrats, it took {} number of tries!", _num);
-
+    readconsole_tofile();
+    readfile();
 }
